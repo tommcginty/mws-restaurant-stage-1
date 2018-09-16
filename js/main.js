08e0e0852d60c777a827df02e7bab51b7b0ddd1b
Tom McGinty <thmcginty@gmail.com>
2018-09-16 15:11:23 -0400

    fix: Ads all urls to serveice worker

--- js/main.js
+++ js/main.js
@@ -174,7 +174,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt', `Image of ${restaurant.name}`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
